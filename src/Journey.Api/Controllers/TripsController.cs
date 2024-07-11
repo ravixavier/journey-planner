58a6bdf823e1cba6fa9ef742012cad860fab035c
Ravi Xavier <ravixavier.dev@gmail.com>
2024-07-10 23:21:42 -0300

    UseCase: Request Register Trip

---
+++ src/Journey.Api/Controllers/TripsController.cs
@@ -0,0 +1,22 @@
+﻿using Journey.Application.UseCases.Trips.Register;
+using Journey.Communication.Requests;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Journey.Api.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class TripsController : ControllerBase
+{
+    [HttpPost]
+    public IActionResult Register([FromBody] RequestRegisterTripJson request)
+    {
+        var useCase = new RegisterTripUseCase();
+        // ou RegisterTripUseCase useCase = new RegisterTripUseCase();
+        // usar 'var' é apenas uma maneira mais fácil de criar uma instancia da variável.
+
+        useCase.Execute(request);
+
+        return Created();
+    }
+}
---
+++ src/Journey.Application/UseCases/Trips/Register/RegisterTripUseCase.cs
@@ -0,0 +1,35 @@
+﻿using Journey.Communication.Requests;
+
+namespace Journey.Application.UseCases.Trips.Register;
+public class RegisterTripUseCase
+{
+    public void Execute(RequestRegisterTripJson request)
+    {
+        Validate(request);
+    }
+
+    private void Validate(RequestRegisterTripJson request)
+    {
+        // quais são as validações que precisamos?
+        // o nome não pode ser vazio, nulo ou espaços em branco.
+        // o .Net já possui um função que verifica isso, string.IsNullOrWhiteSpace
+        // essa função devolve 'true' se por algum acaso a string for vazia ou nula, então lançamos um exeção
+
+        if (string.IsNullOrWhiteSpace(request.Name))
+        {
+            throw new ArgumentException("Nome não pode ser vazio!");
+        }
+
+        // na próxima regra de negócio, eu não quero aceitar um startdate uma data menor que a data atual.
+
+        if (request.StartDate.Date < DateTime.UtcNow.Date)
+        {
+            throw new ArgumentException("Data de início inválida!");
+        }
+
+        if (request.EndDate < request.StartDate)
+        {
+            throw new ArgumentException("Data de retorno inválida!");
+        }
+    }
+}
